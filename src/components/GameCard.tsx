a481de0fedccbf0b0e224240700d46232a54f337
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-01-28 01:20:46 +0530

    ui ux fix for game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,14 +14,13 @@ const GameCard = ({ game }: props) => {
     <Card  >
       <Image src={game.background_image === null ? breoken_image  : game.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms ? game.parent_platforms.map((p) => p.platform) : []}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
