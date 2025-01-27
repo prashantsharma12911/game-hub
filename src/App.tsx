54764cf8aaa090e3fd25f7422bec45f5f35e712a
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-01-28 01:09:51 +0530

    added a dynamic heading for showing seleted filters

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import {
+  Box,
   Button,
   ButtonGroup,
   Grid,
@@ -16,12 +17,13 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGame";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
-  sortOrder : string;
-  searchText : string;
+  sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -39,7 +41,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <Navbar onSearch={(searchText) => setGameQuery({...gameQuery,searchText})}  />
+        <Navbar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
@@ -50,15 +54,23 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatfrom={gameQuery.platform}
-            onSelectPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
-            }
-          />
-          <SortSelector sortOrder = {gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery,sortOrder})} />
-        </HStack>
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector
+              selectedPlatfrom={gameQuery.platform}
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+            />
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
+          </HStack>
+        </Box>
 
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,18 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+import { GameQuery } from '../App'
+
+interface Props{
+    gameQuery : GameQuery;
+}
+
+const GameHeading = ({gameQuery} : Props) => {
+    const heading = `${gameQuery.genre?.name || ''} ${gameQuery.platform?.name || ''} Games`;
+  return (
+    <Heading as='h1' marginY={5}>
+      {heading}
+    </Heading>
+  )
+}
+
+export default GameHeading
