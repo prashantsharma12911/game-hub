62aa265960c33af00efb9509d146842104db8fff
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-01-28 00:18:29 +0530

    build search input in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,13 @@ import logo from '../assets/logo.jpg'
 import { HStack, Icon, Image, Text } from '@chakra-ui/react'
 import ColorModeSwithch from './ColorModeSwitch'
 import { IoGameControllerSharp } from "react-icons/io5";
+import SearchInput from './SearchInput';
 
 const Navbar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
+    <HStack padding='10px'>
         <Icon as={IoGameControllerSharp} boxSize= '60px'></Icon>
+        <SearchInput/>
         <ColorModeSwithch></ColorModeSwithch>
     </HStack>
   )
