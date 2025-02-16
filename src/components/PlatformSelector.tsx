a1f3e9e0d815f438d2fba08064037232d4e50f71
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-02-16 21:49:02 +0530

    fetch platforms using react  query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -18,7 +18,7 @@ const PlatformSelector = ({onSelectPlatform,selectedPlatfrom} : Props) => {
         {selectedPlatfrom?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
+        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
       </MenuList>
     </Menu>
   );
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,8 @@
 import React from 'react'
 import useData from './useData'
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { Response } from './useData';
 
 interface Platform{
     id : number;
@@ -7,6 +10,10 @@ interface Platform{
     slug:string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => useQuery({
+    queryKey : ['platforms'],
+    queryFn : () => apiClient.get<Response<Platform>>('/platforms/lists/parents').then(res => res.data),
+    staleTime : 24 * 60 * 60 * 1000, // 24 hrs
+});
 
 export default usePlatforms;
