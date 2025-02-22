f36de4b9cb9945d98108658b848c617cdf822ea0
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-02-22 19:50:17 +0530

    navigating user to home pages from other page on click of logo or search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,16 @@ import { HStack, Icon, Image, Text } from '@chakra-ui/react'
 import ColorModeSwithch from './ColorModeSwitch'
 import { SlGameController } from "react-icons/sl";
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <HStack padding='10px'>
-        <Icon as={SlGameController} boxSize= '60px'></Icon>
-        <SearchInput/>
-        <ColorModeSwithch></ColorModeSwithch>
+      <Link to='/'>
+        <Icon as={SlGameController} boxSize='60px'></Icon>
+      </Link>
+      <SearchInput />
+      <ColorModeSwithch></ColorModeSwithch>
     </HStack>
   )
 }
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,11 +2,12 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
     const ref = useRef<HTMLInputElement>(null);
     const setSearchText = useGameQueryStore(s => s.setSearchText);  // re-render only when searchText Changes
-
+    const navigate = useNavigate();
 
   return (
 
@@ -14,6 +15,7 @@ const SearchInput = () => {
         event.preventDefault();
         if(ref.current){
             setSearchText(ref.current.value);
+            navigate('/');
         }
     }}>
     <InputGroup >
