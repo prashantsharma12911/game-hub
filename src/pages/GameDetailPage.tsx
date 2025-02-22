5a4ae5b3ed976595d9d0ed9297525a8a8675b5a3
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-02-22 19:44:58 +0530

    split views into two columns for large devices

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useGameDetails from "../hooks/useGameDetail";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import Expandable from "../components/Expandable";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
@@ -18,13 +18,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <Expandable limit={300}>{game.description_raw}</Expandable>
-      <GameAttributes game={game}/>
-      <GameTrailer gameId={game.id}/>
-      <GameScreenshots gameId={game.id}/>
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <Expandable limit={300}>{game.description_raw}</Expandable>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
