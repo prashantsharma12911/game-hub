8827208695e8dec9c51c1f1816a4d7d058af604c
prashant.kumar.12911@gmail.com <prashant.kumar.12911@gmail.com>
2025-02-22 17:50:08 +0530

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from './Genre';
 import { Platform } from './Platform';
+import { Publisher } from './Pulbisher';
 
 
 export interface Game {
@@ -6,6 +8,8 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
+  genres : Genre[];
+  publishers : Publisher[];
   metacritic: number;
   rating_top: number;
   slug: string;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import useGameDetails from '../hooks/useGameDetail';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import Expandable from '../components/Expandable';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useGameDetails from "../hooks/useGameDetail";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import Expandable from "../components/Expandable";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
-    const {slug} = useParams();
-    console.log('slug ',slug?.toString());
-    const {data : game , isLoading , error} = useGameDetails(slug!);  // slug! means we are telling ts compiler that this slug value will never be null
+  const { slug } = useParams();
+  console.log("slug ", slug?.toString());
+  const { data: game, isLoading, error } = useGameDetails(slug!); // slug! means we are telling ts compiler that this slug value will never be null
 
-    if(isLoading) return <Spinner/>
-    if(error || !game) throw error;
-
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
 
   return (
     <>
-        <Heading>{game.name}</Heading>
-        <Expandable limit={300}>{game.description_raw}</Expandable>
+      <Heading>{game.name}</Heading>
+      <Expandable limit={300}>{game.description_raw}</Expandable>
+      <GameAttributes game={game}/>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
